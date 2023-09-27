aa32dd399b93cb4d11a823d694110372a65f0287
Haroon-1ktk <121959975+Haroon-1ktk@users.noreply.github.com>
2023-09-26 22:23:39 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-
+import { useCallback, useState } from "react";
 
 function App() {
+  const [length,setLength]=useState(8);
+  const [numbersallowed,setNumbersallowed]=useState(false);
+   const [charAllowed,setCharAllowed]=useState(false)
+   const [password,setPassword]=useState('')
+
+   const passwordGenerator=useCallback(fn,[])
   return (
    <h1 className='bg-red-500'>Hello</h1>
   );
